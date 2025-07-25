018d6f6aa29268fbe31d4f43ca17f79a6c55668c
ankuranand101 <ankuranandaunta@gmail.com>
2025-07-25 23:13:55 +0530

    Initial commit

---
+++ home_gym/src/utils/fetchData.js
@@ -0,0 +1,26 @@
+export const exerciseOptions = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
+    'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
+  },
+};
+
+export const youtubeOptions = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com',
+    'X-RapidAPI-Key': 'f0021db587msh781fb1cbef39856p11c183jsn45521d5d1c85',
+  },
+};
+
+export const fetchData = async (url, options) => {
+  try {
+    const response = await fetch(url, options);
+    const data = await response.json(); // 👈 this must return an array
+    return data;
+  } catch (error) {
+    console.error('Fetch error:', error);
+    return []; // fallback to empty array to avoid "not iterable"
+  }
+};
---
+++ project_fitness_app-main/src/utils/fetchData.js
@@ -0,0 +1,22 @@
+export const exerciseOptions = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
+    'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
+  },
+};
+
+export const youtubeOptions = {
+  method: 'GET',
+  headers: {
+    'x-rapidapi-key': '97f3417eccmshec28116fe946f9dp172e95jsn5e578f7e7967',
+    'x-rapidapi-host': 'youtube-search-and-download.p.rapidapi.com',
+  },
+};
+
+export const fetchData = async (url, options) => {
+  const res = await fetch(url, options);
+  const data = await res.json();
+
+  return data;
+};
